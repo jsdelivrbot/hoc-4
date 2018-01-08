c3b4d811e2e5118a61e764849d5f83bc1287b284
mao <tonytonitone6@gmail.com>
2018-01-09 01:26:29 +0800

    1.09

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import * as actions from '../actions';
 
 
 class Header extends Component {
   
 
   authButton = () => {
-    return (
-      <div>
-        <button className="btn btn-danger">
-          Sign In
-        </button>
-      </div>
-    )
+    if (this.props.authenticated) {
+      return (
+        <div>
+          <button className="btn btn-danger" onClick={() => this.props.authenticate(false)}>
+            Sign Out
+          </button>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <button className="btn btn-danger" onClick={() => this.props.authenticate(true)}>
+            Sign In
+          </button>
+        </div>
+      )
+    }
   }
 
   render() {
@@ -35,4 +46,10 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({authenticated}) {
+  return {
+    authenticated
+  }
+}
+
+export default connect(mapStateToProps, actions)(Header);
